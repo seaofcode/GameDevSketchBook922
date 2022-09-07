f0dca0216ae5d9228649b13b93aecd2c76172cfb
Matthew J Pearson <seaofcode@gmail.com>
2022-09-07 12:37:34 -0500

    Color Change Switch

---
+++ Assets/03-Gameplay/Scripts/ColorChangeSwitch.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeSwitch : MonoBehaviour
+{
+    [SerializeField] bool isBlue;
+    [SerializeField] bool isGreen;
+    [SerializeField] bool isRed;
+
+    private SpriteRenderer spriteRenderer;
+
+    public GameManager gameManager;
+    public bool isCorrectColor;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if(isBlue){
+            spriteRenderer.color = Color.blue;
+        }
+        if(isGreen){
+            spriteRenderer.color = Color.green;
+        }
+        if(isRed){
+            spriteRenderer.color = Color.red;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Player")
+        {
+            TurnColor();
+        }
+    }
+
+    void TurnColor()
+    {
+        if(isBlue){
+            spriteRenderer.color = Color.red;
+            isBlue = false;
+            isRed = true;
+            isCorrectColor = false;
+        }
+        else if(isRed){
+            spriteRenderer.color = Color.green;
+            isRed = false;
+            isGreen = true;
+            isCorrectColor = true;
+            gameManager.CorrectColorCounter();
+        }
+        else if(isGreen){
+            spriteRenderer.color = Color.blue;
+            isGreen = false;
+            isBlue = true;
+            isCorrectColor = false;
+        }
+    }
+}
--- Assets/03-Gameplay/Scripts/GameManager.cs
+++ Assets/03-Gameplay/Scripts/GameManager.cs
@@ -7,6 +7,24 @@ public class GameManager : MonoBehaviour
     [SerializeField] int scoreCount = 0;
     [SerializeField] SwitchDoor mySwitchDoor;
 
+    [SerializeField] ColorChangeSwitch greenSwitch1;
+    [SerializeField] ColorChangeSwitch greenSwitch2;
+    [SerializeField] ColorChangeSwitch greenSwitch3;
+
+    
+    void Start() {
+        greenSwitch1 = GameObject.Find("ColorChangeSwitch1").GetComponent<ColorChangeSwitch>();
+        greenSwitch2 = GameObject.Find("ColorChangeSwitch2").GetComponent<ColorChangeSwitch>();
+        greenSwitch3 = GameObject.Find("ColorChangeSwitch3").GetComponent<ColorChangeSwitch>();
+    }
+
+    public void CorrectColorCounter()
+    {
+        if(greenSwitch1.isCorrectColor && greenSwitch2.isCorrectColor && greenSwitch3.isCorrectColor)
+        {
+            mySwitchDoor.OpenDoor();
+        }
+    }
 
     public void increaseCount(){
         scoreCount++;
